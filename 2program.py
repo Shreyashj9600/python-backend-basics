bfeb0f9d3b6df838607beeb552ff502159cfe504
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-21 17:53:12 +0530

    input in python

---
+++ 2program.py
@@ -0,0 +1,11 @@
+# Input in python
+
+# input() statement is used to accept values (using keybord) from user
+
+# string input 
+name = input("name : ")
+print(name)
+
+# Int input
+age = int(input("age : " ))
+print(age)
