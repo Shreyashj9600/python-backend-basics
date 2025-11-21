6a015c8d81abedca1f36957a48bddfd7f41e27f0
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 00:01:35 +0530

    conditional statements

--- 2program.py
+++ 2program.py
@@ -2,10 +2,33 @@
 
 # input() statement is used to accept values (using keybord) from user
 
-# string input 
-name = input("name : ")
-print(name)
+# # string input 
+# name = input("name : ")
+# print(name)
 
-# Int input
-age = int(input("age : " ))
-print(age)
+# # Int input
+# age = int(input("age : " ))
+# print(age)
+
+# conditional statement
+# if-elif-else
+
+#if(condition) :
+    #statement1
+#elif(condition) :
+    #statement2    
+#else:
+    #statement      
+
+# trafic light example    
+
+light = input("light :")
+
+if(light == "red") :
+    print('Stop')
+elif(light == "yello") :
+    print('look')
+elif(light == "green") :
+    print('go')
+else: 
+    print("light is broken ")    
