cb65aadfdbe095df68e4a86c28fdd718238dc11e
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 00:54:36 +0530

    Type casting

---
+++ 4program.py
@@ -0,0 +1,20 @@
+# Type Conversion
+a = 2 
+b = 4.25
+
+sum = a + b 
+print(sum)
+
+
+# Type casting
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(a + b)
+
+a = 3.34
+
+a = str(a)
+print(type(a))
+print(a)
