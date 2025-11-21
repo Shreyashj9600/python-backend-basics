8405c37df808ff98daf048611a770a70e112f953
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-21 15:46:53 +0530

    Data types in python

--- firstProgram.py
+++ firstProgram.py
@@ -11,3 +11,16 @@ print('my email is' , email)
 # type chake in python
 print(type(name))
 print(type(email))
+
+# Data types in python
+# 1) Integers : +ve, -ve (5, -5, 0)
+# 2) String : 'shreyash' , "shreyash" , '''shreyash'''  (' ', " ", ''' ''', )
+# 3)Float : 2.99, 5.87
+#  4) Boolean : True, false
+#  5) None : a = None   
+
+old = True
+a = None
+print(type(old))
+print(type(a))
+
