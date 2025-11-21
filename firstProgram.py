ed25725a3842facf7a02748d6b129168a5f1931e
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-21 15:53:36 +0530

    Resived keyword in python

--- firstProgram.py
+++ firstProgram.py
@@ -24,3 +24,14 @@ a = None
 print(type(old))
 print(type(a))
 
+# Keywords
+# keyword are reserved word in python
+
+# False      await      else       import     pass
+# None       break      except     in         raise
+# True       class      finally    is         return
+# and        continue   for        lambda     try
+# as         def        from       nonlocal   while
+# assert     del        global     not        with
+# async      elif       if         or         yield
+
