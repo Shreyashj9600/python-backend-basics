003636d2f864827be39c9fab93d2a0f02109de68
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 11:30:19 +0530

    list in python

---
+++ chapter 3/1program.py
@@ -0,0 +1,15 @@
+# Lists in Python 
+# A built-in data type that stores set of values
+# it can store element of different types(integer,  float, string, etc.)
+
+marks = [12,23,35,65,53]
+print(marks)
+print(type(marks))
+
+list = [2,1,3]
+list.append(4) # add one element at the end [2,1,3,4]
+list.sort() # sort in ascending order [1,2,3]
+list.sort(reverse=True) # sort in descending order [3,2,1]
+list.reverse() # reverses list [3,1,2]
+#list.insert(idx,el) insert element at the index 
+
