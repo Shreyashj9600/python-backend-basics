f32801d3e342ed0eff849389e96dba0995b1b96b
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 13:08:53 +0530

    tupel in python

---
+++ chapter 3/2program.py
@@ -0,0 +1,11 @@
+# Tuples in python
+# a built-in-data type lets us create immutable sequence of values.
+
+tup = (2,1,3,1)
+print(type(tup))
+print(tup[0])
+# tup[0] = 5 tuple object does not support item assignment
+
+# tupel method
+tup.index() # return index of first occurrence
+tup.count() # counts total occurrences 
