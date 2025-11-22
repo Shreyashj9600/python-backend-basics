845bb6614b1dd8acb1e59e9dc02bbdf9e928c765
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 20:57:17 +0530

    range() in python

--- chapter 5/2program.py
+++ chapter 5/2program.py
@@ -25,3 +25,4 @@ while i <= 10:
     print(i)
     i += 1    
     
+    
---
+++ chapter 5/3program.py
@@ -0,0 +1,37 @@
+# Loops in Python 
+# Loops are used for sequential traversal,  for traversing list string tuples etc.
+
+nums = [1,2,3,4,5]
+
+for num in nums :
+    print("printing numbers ", num)
+
+
+str = "ApnaCollage"
+for ch in str:
+    print(ch)
+else:
+    print("END")    
+
+nums = (1,4,9,16,25,36,49,64,81,100,49)
+x = 49
+ind = 0
+for el in nums:
+    if(el == x):
+        print("Index Foun" , ind)
+    ind += 1
+
+
+# Range
+for i in range(5): # range(stop)
+    print(i)
+
+print('----------------')
+
+for i in range(2,10): # range(start,stop)
+    print(i)
+
+print('----------------')
+
+for i in range(2,10,2) : # range(start,stop, step)
+    print(i)
