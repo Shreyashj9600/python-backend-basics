25f41c26da428dac0fde9786288ee2620318e804
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-22 10:19:28 +0530

    string in python

---
+++ chapter 2/1program.py
@@ -0,0 +1,34 @@
+# Strings
+# string is a data type that store a sequence of charactor
+
+# Basic operation 
+# concatenation
+    # "hello" + "world" --> "helloworld"
+
+str1 = 'hello'
+str2 = 'world'
+
+final_string = str1 + str2
+print(final_string)
+
+# length of str
+len1 = "shreyash"
+result = len(len1)
+print(result)
+
+# Indexing
+# h e l l o _ w o r  l  d
+# 1 2 3 4 5 6 7 8 9 10 11
+
+str = "Apna collage"
+ch = str[2]
+print(ch)
+
+# Slicing 
+# Accesing part of string
+# str[starting_ind : ending_ind] # ending idx is not included
+
+str2 = 'Apna Collage'
+print(str2[1:4])
+print(str2[:4]) #[0:4]
+print(str2[5:]) #[5:len(str2)]
