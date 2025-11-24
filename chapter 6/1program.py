4f22942b64ef801509f5860197bb6367fdc883af
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-25 00:21:45 +0530

    function in python

---
+++ chapter 6/1program.py
@@ -0,0 +1,13 @@
+# Function in python 
+# Block of statements that preform a specific task
+
+def caculate_sum (num1, num2):  
+    sum = num1 + num2
+    print(sum)
+
+caculate_sum(5,4)         
+
+def print_hello():
+    print("Hello world")
+
+print_hello()    
