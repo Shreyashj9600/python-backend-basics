2ab77cbe2fea9b321dfb132c9ad583dae0a79227
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-25 11:11:11 +0530

    functions & recursion

--- chapter 6/1program.py
+++ chapter 6/1program.py
@@ -11,3 +11,19 @@ def print_hello():
     print("Hello world")
 
 print_hello()    
+
+# solving que
+
+names = ["Rahul", "Amit", "Sneha", "Pooja", "Ravi"]
+
+def print_name(list) :
+    for el in list:
+        print(el)
+
+print_name(names)        
+
+def converter(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val,"USD = ", inr_val , "INR")
+
+converter(10)   
